64cd957614385d4ada5fc1e4e5183eb5bb55ad63
Mugaty7 <10000540.joan23@fje.edu>
2022-11-14 15:25:35 +0100

    bd actualizada

--- sql/bd_dooku - buena.sql
+++ sql/bd_dooku - buena.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-11-2022 a las 15:28:52
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 7.4.30
+-- Tiempo de generación: 14-11-2022 a las 15:20:13
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,6 +26,24 @@ USE `bd_dooku`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tbl_incidencia`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_incidencia` (
+  `id_inc` int(11) NOT NULL AUTO_INCREMENT,
+  `desc_inc` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `id_man_fk` int(11) NOT NULL,
+  `id_user_fk` int(11) NOT NULL,
+  `id_mesa_fk` int(11) NOT NULL,
+  PRIMARY KEY (`id_inc`),
+  KEY `id_man_fk` (`id_man_fk`),
+  KEY `id_user_fk` (`id_user_fk`),
+  KEY `id_mesa_fk` (`id_mesa_fk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tbl_man`
 --
 
@@ -36,14 +54,15 @@ CREATE TABLE IF NOT EXISTS `tbl_man` (
   `username` varchar(10) COLLATE utf8mb4_spanish_ci NOT NULL,
   `password` varchar(256) COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id_man`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_man`
 --
 
 INSERT INTO `tbl_man` (`id_man`, `nombre_man`, `dni`, `username`, `password`) VALUES
-(1, 'uiliam', '54987812X', 'luquitas11', '5d56fed4e243d657c6ae1af13adb709690cd7c51');
+(1, 'uiliam', '54987812X', 'luquitas11', '5d56fed4e243d657c6ae1af13adb709690cd7c51'),
+(2, 'Alex', '46488089Y', 'alexm', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
 
 -- --------------------------------------------------------
 
@@ -98,24 +117,32 @@ CREATE TABLE IF NOT EXISTS `tbl_reserva` (
   `telefono_persona` char(9) COLLATE utf8mb4_spanish_ci NOT NULL,
   `hora_inici` datetime NOT NULL DEFAULT current_timestamp(),
   `hora_fi` datetime DEFAULT NULL,
+  `duracion` time DEFAULT NULL,
   PRIMARY KEY (`id_reserva`),
   KEY `id_user` (`id_user`),
   KEY `id_mesa` (`id_mesa`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_reserva`
 --
 
-INSERT INTO `tbl_reserva` (`id_reserva`, `id_user`, `id_mesa`, `nom_persona`, `apellido_persona`, `telefono_persona`, `hora_inici`, `hora_fi`) VALUES
-(1, 1, 1, 'Pedro', '', '', '2022-11-07 18:19:14', '2022-11-10 15:26:00'),
-(2, 2, 1, 'Pedro', '', '', '2022-11-07 18:19:26', '2022-11-10 15:26:00'),
-(3, 1, 2, 'asdas', 'das', '654423432', '2022-11-09 18:36:55', NULL),
-(4, 1, 1, 'Funcioan', 'profa', '543123121', '2022-11-09 18:37:44', '2022-11-10 15:26:00'),
-(5, 1, 3, 'Italo', 'Marques', '549878121', '2022-11-09 19:30:03', '2022-11-09 19:31:35'),
-(6, 1, 3, 'asdasd', 'asdasd', '123456789', '2022-11-09 19:30:58', '2022-11-09 19:31:35'),
-(7, 1, 1, 'werqwe', 'werqwer', '12345678', '2022-11-09 19:31:20', '2022-11-10 15:26:00'),
-(8, 1, 1, 'Testeo', 'Intesivo', '4351234', '2022-11-10 15:25:23', '2022-11-10 15:26:00');
+INSERT INTO `tbl_reserva` (`id_reserva`, `id_user`, `id_mesa`, `nom_persona`, `apellido_persona`, `telefono_persona`, `hora_inici`, `hora_fi`, `duracion`) VALUES
+(1, 1, 1, 'Pedro', '', '', '2022-11-07 18:19:14', '2022-11-10 19:40:57', '00:00:00'),
+(2, 2, 1, 'Pedro', '', '', '2022-11-07 18:19:26', '2022-11-10 19:40:57', '00:00:00'),
+(3, 1, 2, 'asdas', 'das', '654423432', '2022-11-09 18:36:55', '2022-11-10 19:36:28', '00:00:00'),
+(4, 1, 1, 'Funcioan', 'profa', '543123121', '2022-11-09 18:37:44', '2022-11-10 19:40:57', '00:00:00'),
+(5, 1, 3, 'Italo', 'Marques', '549878121', '2022-11-09 19:30:03', '2022-11-09 19:31:35', '00:00:00'),
+(19, 1, 1, 'asd', 'asda', '123456789', '2022-11-11 15:20:46', '2022-11-11 18:16:21', '02:55:35'),
+(20, 1, 2, 'Josejuan', 'asdad', '123456789', '2022-11-11 15:20:57', NULL, '00:00:00'),
+(21, 1, 3, 'Manel', 'Garcia Moreno', '676272272', '2022-11-11 15:21:26', NULL, '00:00:00'),
+(22, 1, 15, 'Alex', 'Garcia Moreno', '123123123', '2022-11-11 17:55:25', '2022-11-11 17:56:03', '00:00:38'),
+(23, 1, 3, 'Manel', 'Muga Lopez', '123123123', '2022-11-11 17:58:57', NULL, '00:00:00'),
+(24, 1, 5, 'Alex', 'Muga', '123123123', '2022-11-11 18:00:46', '2022-11-11 18:00:59', '00:00:13'),
+(25, 1, 1, 'asdsa', 'assda', '123123123', '2022-11-11 18:10:53', '2022-11-11 18:16:21', '00:05:28'),
+(26, 1, 1, 'asda', 'asd', '123123123', '2022-11-11 18:12:13', '2022-11-11 18:16:21', '00:04:08'),
+(27, 1, 1, 'asda', 'asda', '123123123', '2022-11-11 18:16:38', NULL, NULL),
+(28, 1, 1, 'Alex', 'Muga Lopez', '123321123', '2022-11-11 18:20:32', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -146,6 +173,14 @@ INSERT INTO `tbl_user` (`id_user`, `nombre_user`, `dni`, `username`, `password`)
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `tbl_incidencia`
+--
+ALTER TABLE `tbl_incidencia`
+  ADD CONSTRAINT `tbl_incidencia_ibfk_1` FOREIGN KEY (`id_user_fk`) REFERENCES `tbl_user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_incidencia_ibfk_2` FOREIGN KEY (`id_man_fk`) REFERENCES `tbl_man` (`id_man`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_incidencia_ibfk_3` FOREIGN KEY (`id_mesa_fk`) REFERENCES `tbl_mesa` (`id_mesa`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `tbl_reserva`
